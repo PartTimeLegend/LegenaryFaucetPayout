aa6db78276d3254974e9ed804855c519ec40c460
Antony <Bailey>
2013-03-19 00:54:17 +0000

    Init
    Probably doesn't work, I'm going to bed.

---
+++ FaucetPayout/frmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FaucetPayout
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPayout_Click(object sender, EventArgs e)
+        {
+            var ll = new LogicLayer.WebRequests();
+            lblStatus.Text = ll.Payout().ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ LogicLayer/WebRequests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Net.Sockets;
+using System.Web.Handlers;
+
+namespace LogicLayer
+{
+    public class WebRequests
+    {
+        public string Payout()
+        {
+            var client = new HttpClient();
+            var requestContent = new FormUrlEncodedContent(new [] {
+            new KeyValuePair<string, string>("secret", "YourSecretKeyHere"),});
+            var response = client.PostAsJsonAsync("http://legendaryfaucet.herokuapp.com", requestContent);
+            return response.ToString();
+            
+        }
+    }
+}
